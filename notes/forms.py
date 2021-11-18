a660cf287b907710f29c26d278c72d04c8e0b5c1
jonathan10620 <jonathan10620@gmail.com>
2021-11-18 12:56:34 -0600

    added date validation for note form

--- notes/forms.py
+++ notes/forms.py
@@ -1,19 +1,32 @@
 from django import forms
 from .models import Note
 from datetime import datetime
+from django.utils import timezone
+
 
 class NoteForm(forms.ModelForm):
-    date = forms.DateTimeField(widget=forms.widgets.DateTimeInput(attrs={'type': 'datetime-local', "class": "form-control form-control-sm"}))
-    body = forms.CharField(widget=forms.Textarea(attrs={'class':'form-control', 'rows':'3'}))
-    
+    def no_future_date(value):
+        if value > timezone.now():
+            raise forms.ValidationError("Date and time cannot be in the future")
+
+    date = forms.DateTimeField(
+        validators=[no_future_date],
+        widget=forms.widgets.DateTimeInput(
+            attrs={"type": "datetime-local", "class": "form-control form-control-sm"}
+        ),
+    )
+    body = forms.CharField(
+        widget=forms.Textarea(attrs={"class": "form-control", "rows": "3"})
+    )
+
     author = forms.CharField(
         widget=forms.TextInput(attrs={"class": "form-control form-control-sm"})
     )
 
     def __init__(self, *args, **kwargs):
         super(NoteForm, self).__init__(*args, **kwargs)
-        self.fields['date'].initial = datetime.today().strftime('%Y-%m-%dT%H:%M')
+        self.fields["date"].initial = datetime.today().strftime("%Y-%m-%dT%H:%M")
 
     class Meta:
         model = Note
-        fields = ['date', 'body', 'author']
+        fields = ["date", "body", "author"]
