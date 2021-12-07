099b10b3b3045a4ba1cca2b11079fe8202a80497
jonathan10620 <jonathan10620@gmail.com>
2021-12-06 21:28:28 -0600

    working on intervention page

--- interventions/models.py
+++ interventions/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class intervention(models.Model):
+    pass
+
+
+class FeedingEntries(models.Model):
+    initials = models.CharField(max_length=100)
+    time_given = models.TimeField()
+    int_day = models.IntegerField()
--- interventions/views.py
+++ interventions/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from .models import FeedingEntries
 
 # Create your views here.
 def interventions(request):
     if not request.session.get("logged_in"):
         messages.add_message(request, messages.INFO, "Please log in first")
         return redirect("login")
+    
+    context = {}
 
-    return render(request, 'interventions/interventions.html')
+    return render(request, 'interventions/interventions.html', context)
